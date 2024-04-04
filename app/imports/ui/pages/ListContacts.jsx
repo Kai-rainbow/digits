0b26e885ad6e4c4f90a838c35860f7548a3322f8
Kai-rainbow <kaitot@hawaii.edu>
2024-04-03 20:42:54 -1000

    clean up

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -31,7 +31,7 @@ const ListContacts = () => {
   return (ready ? (
     <Container className="py-3">
       <Row className="justify-content-center">
-        <Col md={7}>
+        <Col md={10}>
           <Col className="text-center">
             <h2>List Contacts</h2>
           </Col>
